69443e16e1d255ebcfd22f9b85943d5017fb99c9
林志航 <2205358329@qq.com>
2017-07-11 11:32:55 +0800

    代码格式化

--- HitokotoCrawler.py
+++ HitokotoCrawler.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
 import requests
 import os
-counter=1
-list1=[]
-time=int(input('输入请求次数\n'))
-while counter<=time:
-    print ('当前正在请求第 %d 次，共 %d 次' % (counter,time))
+counter = 1
+list1 = []
+time = int(input('输入请求次数\n'))
+while counter <= time:
+    print('当前正在请求第 %d 次，共 %d 次' % (counter, time))
     gethitokoto = requests.get('https://sslapi.hitokoto.cn/?encode=text')
-    text=gethitokoto.text
-    textlong=len(text)
-    if textlong<=100:
+    text = gethitokoto.text
+    textlong = len(text)
+    if textlong <= 100:
         list1.append(text)
-        counter=counter+1
+        counter = counter + 1
     else:
-        print ('该字符串不符合要求，已经舍去，继续尝试下一次操作..')
+        print('该字符串不符合要求，已经舍去，继续尝试下一次操作..')
 else:
-    print ('请求已完成')
-    input ('按下Enter以列出请求数据,并把数据写入 output.txt')
-    file=open('output.txt','a')
-    print ('\n')
+    print('请求已完成')
+    input('按下Enter以列出请求数据,并把数据写入 output.txt')
+    file = open('output.txt', 'a')
+    print('\n')
     for output in list1:
-        print (output)
+        print(output)
         file.write(output + '\n')
-    print ('\n')
+    print('\n')
     file.close()
-    input ('按下Enter键以退出')
+    input('按下Enter键以退出')
